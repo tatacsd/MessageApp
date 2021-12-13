122e12bd9c19e7cd63b2cf886c8896c4e5383f00
Thays Casado <thayscasado@hotmail.com>
2021-12-12 20:20:31 -0800

    firebase

--- app/src/main/java/com/thayscasado/messageapp/MainActivity.java
+++ app/src/main/java/com/thayscasado/messageapp/MainActivity.java
@@ -4,11 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+
+    // Reference to firebase database
+    FirebaseDatabase mfirebaseIntance;
+    DatabaseReference mfirebaseDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+
     }
 }
